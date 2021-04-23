0f02dab269c2f1e976b0e1eb3d77ee4386ffdcac
CelsoTaliatelli <celsotaliatelli@hotmail.com>
2021-04-23 12:52:41 -0300

    add orcacle client

--- app/index.php
+++ app/index.php
@@ -1,2 +1,20 @@
 <?php
-    phpinfo();
+
+$tns = "(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = 192.168.0.76)(PORT = 1521))(CONNECT_DATA =(SERVER = DEDICATED)(SERVICE_NAME = PROD)))";
+
+try{
+    $conn = new PDO("oci:dbname=".$tns,'tasy','aloisk');
+}catch(PDOException $e){
+    var_dump($e);
+}
+
+$qTotalOsAbertas = "select 
+                   COUNT(*) TOTAL
+                    from MAN_ORDEM_SERVICO
+                    where IE_STATUS_ORDEM = 1 AND NR_GRUPO_PLANEJ = 22
+                     AND NR_GRUPO_TRABALHO = 38
+                    order by dt_ordem_servico";
+
+$exibeTotalOsAbertas = $conn->query($qTotalOsAbertas)->fetchAll();
+
+var_dump($exibeTotalOsAbertas);
